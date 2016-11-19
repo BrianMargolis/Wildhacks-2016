28a06fdff3236baedbafc7d7ade5d1134e2e8587
Brian Margolis <Brian.M.Margolis@gmail.com>
2016-11-19 13:04:16 -0600

    Added library file

---
+++ library.js
@@ -0,0 +1,15 @@
+// Returns a random color. Should we make this just select from a predefined list, so we can restrict what colors the circle is?
+function getRandomColor() {
+    var letters = '0123456789ABCDEF';
+    var color = '#';
+    for (var i = 0; i < 6; i++) {
+        color += letters[Math.floor(Math.random() * 16)];
+    }
+    return color;
+}
+
+// Makes the circle change colors a little less aggressively
+function shouldUpdateColor() {
+    var param = .95; // higher values (must be < 1) make it change less; lower values (must be > 0) make it change more
+    return (Math.random() > param);
+}
