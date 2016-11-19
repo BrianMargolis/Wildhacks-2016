ba585b7af65f200993a9f9f2f794947305bfb59d
Brian Margolis <Brian.M.Margolis@gmail.com>
2016-11-18 20:39:06 -0600

    All sorts of shit that doesn't work

--- move.js
+++ move.js
@@ -12,3 +12,40 @@ function move() {
         }
     }
 }
+//
+// function getMousePos(container, evt) {
+//     var rect = container.getBoundingClientRect();
+//     return {
+//         x: evt.clientX - rect.left,
+//         y: evt.clientY - rect.top
+//     };
+// }
+//
+// var container = document.getElementById('container');
+//
+// container.addEventListener('mousemove', function(evt) {
+//     var mousePos = getMousePos(container, evt);
+//     moveBox(animate, mousePos.x, mousePos.y);
+// }, false);
+//
+// function moveBox(box, x, y) {
+//     var elem = document.getElementById("animate");
+//     // context.clearRect(0, 0, container.width, container.height);
+//     elem.style.top = y + 'px';
+//     elem.style.left = x + 'px';
+// }
+
+function getMousePos(canvas, evt) {
+    var rect = canvas.getBoundingClientRect();
+    return {
+        x: evt.clientX - rect.left,
+        y: evt.clientY - rect.top
+    };
+}
+var container = document.getElementById('container');
+
+container.addEventListener('mousemove', function(evt) {
+    var mousePos = getMousePos(container, evt);
+    var message = 'Mouse position: ' + mousePos.x + ',' + mousePos.y;
+    writeMessage(container, message);
+}, false);
